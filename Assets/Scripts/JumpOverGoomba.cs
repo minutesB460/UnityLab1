d959ff3e77bfc6efc79f5c5a8c6464502b8ca27c
wangyongjie <wangyongje717@gmail.com>
2025-02-06 06:04:59 +0800

    Updated scripts

---
+++ Assets/Scripts/JumpOverGoomba.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class JumpOverGoomba : MonoBehaviour
+{
+
+    public Transform enemyLocation;
+    public TextMeshProUGUI scoreText;
+    private bool onGroundState;
+
+    [System.NonSerialized]
+    public int score = 0; // we don't want this to show up in the inspector
+
+    private bool countScoreState = false;
+    public Vector3 boxSize;
+    public float maxDistance;
+    public LayerMask layerMask;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("On Ground: " + onGroundState);
+        // mario jumps
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            onGroundState = false;
+            countScoreState = true;
+        }
+
+        // when jumping, and Goomba is near Mario and we haven't registered our score
+        if (!onGroundState && countScoreState)
+        {
+            score++;
+            if (Mathf.Abs(transform.position.x - enemyLocation.position.x) < 0.5f)
+            {
+                countScoreState = false;
+                score++;
+                scoreText.text = "Score: " + score.ToString();
+                Debug.Log(score);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    // void OnCollisionExit2D(Collision2D col)
+    // {
+    //     if (col.gameObject.CompareTag("Ground"))
+    //     {
+    //         onGroundState = false;
+    //         Debug.Log("Not on Ground");
+    //     }
+    // }
+
+
+    private bool onGroundCheck()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, maxDistance, layerMask))
+        {
+            Debug.Log("on ground");
+            return true;
+        }
+        else
+        {
+            Debug.Log("not on ground");
+            return false;
+        }
+    }
+}
+
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10;
     private Rigidbody2D marioBody;
+
+    //Variable for Impulse force upwards
+    public float upSpeed = 10;
+    private bool onGroundState = true;
+    public float maxSpeed = 20;
+
+
+    // global variables
+    private SpriteRenderer marioSprite;
+    private bool faceRightState = true;
+
+    public TextMeshProUGUI scoreText;
+    public GameObject enemies;
+
+    public GameObject gameOverUI;
 
     // Start is called before the first frame update
     void Start()
@@ -13,17 +30,32 @@ public class PlayerMovement : MonoBehaviour
         // Set to be 30 FPS
         Application.targetFrameRate = 30;
         marioBody = GetComponent<Rigidbody2D>();
+        marioSprite = GetComponent<SpriteRenderer>();
+
+        gameOverUI.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Flip Mario based on diff keys
+        // Do not implement the flipping of Sprite under FixedUpdate since it has nothing to do with the Physics Engine.
+        // toggle state
+        if (Input.GetKeyDown("a") && faceRightState)
+        {
+            faceRightState = false;
+            marioSprite.flipX = true;
+        }
 
+        if (Input.GetKeyDown("d") && !faceRightState)
+        {
+            faceRightState = true;
+            marioSprite.flipX = false;
+        }
     }
 
     // FixedUpdate is called 50 times a second
-    public float maxSpeed = 20;
 
     // FixedUpdate may be called once per frame. See documentation for details.
     void FixedUpdate()
@@ -44,6 +76,60 @@ public class PlayerMovement : MonoBehaviour
             // stop
             marioBody.linearVelocity = Vector2.zero;
         }
+
+        //If spacebar is pressed, we will add an Impulse force upwards.
+        if (Input.GetKeyDown("space") && onGroundState)
+        {
+            marioBody.AddForce(Vector2.up * upSpeed, ForceMode2D.Impulse);
+            onGroundState = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground")) onGroundState = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with goomba!");
+            Time.timeScale = 0.0f;
+
+            gameOverUI.SetActive(true);
+        }
+    }
+
+    public void RestartButtonCallback(int input)
+    {
+        Debug.Log("Restart!");
+        // reset everything
+        ResetGame();
+        // resume time
+        Time.timeScale = 1.0f;
+    }
+
+    private void ResetGame()
+    {
+
+        // reset position
+        marioBody.transform.position = new Vector3(-3f, -0.72f, 0.0f);
+        // reset sprite direction
+        faceRightState = true;
+        marioSprite.flipX = false;
+        // reset score
+        scoreText.text = "Score: 0";
+        // reset Goomba
+        foreach (Transform eachChild in enemies.transform)
+        {
+            eachChild.transform.localPosition = eachChild.GetComponent<EnemyMovement>().startPosition;
+        }
+
+
+
     }
 
 }
+
+
+
